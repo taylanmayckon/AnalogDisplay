19bc99f322da00b32b10c43df123f9a8309422ed
Taylan <eusoutaylann@gmail.com>
2025-02-18 01:21:47 -0300

    Add: Display I2C com o quadrado 8x8 que se move

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
 #include "hardware/pwm.h"
+#include "hardware/i2c.h"
+#include "libs/ssd1306.h"
+#include "libs/font.h"
 
 #define JOYSTICK_X 27
 #define JOYSTICK_Y 26
@@ -14,10 +17,17 @@
 #define OUTPUT_MASK ((1 << LED_BLUE) | (1 << LED_RED) | (1 << LED_GREEN))
 #define INPUT_MASK ((1 << JOYSTICK_BUTTON) | (1 << BUTTON_A))
 
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define DISPLAY_ADDRESS 0x3C
+
 uint blue_pwm_slice, red_pwm_slice;
 uint wrap = 2047;
 uint32_t last_time = 0;
 bool pwm_state = true;
+
+ssd1306_t ssd; // Inicializa a estrutura do display no escopo global
 
 
 // Função de interrupção da GPIO
@@ -58,8 +68,44 @@ uint choice_pwm_side(uint joy_input){
     }
 }
 
+int choice_display_y(int joy_input){
+    if(joy_input < 2048){
+        return (2047-joy_input)/(2047/27);
+    }
+    else{
+        return -(joy_input-2048)/(2047/27);
+    }
+}
+
+int choice_display_x(int joy_input){
+    if(joy_input < 2048){
+        return -(2047-joy_input)/(2047/27);
+    }
+    else{
+        return (joy_input-2048)/(2047/27);
+    }
+}
+
 int main(){
     stdio_init_all();
+
+    // Inicializando o I2C
+    i2c_init(I2C_PORT, 400*1000);
+    // Setando as funções dos pinos do I@C
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    // Garantindo o Pull up do I2C
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+
+    // Configuração do display
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, DISPLAY_ADDRESS, I2C_PORT); // Inicializa o display
+    ssd1306_config(&ssd); // Configura o display
+    ssd1306_send_data(&ssd); // Envia os dados para o display
+    // Limpa o display. O display inicia com todos os pixels apagados.
+    ssd1306_fill(&ssd, false);
+    ssd1306_send_data(&ssd);
+    bool cor = true; // Booleano que indica a cor branca do pixel
 
     adc_init(); // Inicializando o ADC
     adc_gpio_init(JOYSTICK_X); // Canal 1
@@ -100,7 +146,17 @@ int main(){
         pwm_set_gpio_level(LED_BLUE, choice_pwm_side(vry_value)); // Eixo Y - Azul
         pwm_set_gpio_level(LED_RED, choice_pwm_side(vrx_value)); // Eixo X - Vermelho
         
-        printf("Duty X: %d | Duty Y: %d\n", choice_pwm_side(vrx_value)*100/2047, choice_pwm_side(vry_value)*100/2047); // Print para debug
+        //printf("Duty X: %d | Duty Y: %d\n", choice_pwm_side(vrx_value)*100/2047, choice_pwm_side(vry_value)*100/2047); // Print para debug
+
+        // Limpa o display
+        ssd1306_fill(&ssd, false);
+        // Atualização do display
+        ssd1306_rect(&ssd, 0, 0, 127, 63, cor, !cor); // Desenha um frame nos contornos do display
+        ssd1306_draw_char(&ssd, '*', 59+choice_display_x(vrx_value), 27+choice_display_y(vry_value)); // Quadrado preenchido
+
+        printf("Display x: %d | Display Y: %d\n", choice_display_x(vrx_value), choice_display_y(vry_value));
+
+        ssd1306_send_data(&ssd); // Atualiza o display
 
         sleep_ms(10);
     }
