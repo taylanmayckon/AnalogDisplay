2ce9fad1588c894bb4e33ca0abb3bc2f78fc86fc
Taylan <eusoutaylann@gmail.com>
2025-02-17 21:42:18 -0300

    Add: Alternância do estado dos LEDs com botões

--- main.c
+++ main.c
@@ -5,17 +5,47 @@
 
 #define JOYSTICK_X 27
 #define JOYSTICK_Y 26
+#define JOYSTICK_BUTTON 22
+#define BUTTON_A 5
 #define LED_BLUE 12
 #define LED_RED 13
+#define LED_GREEN 11
 
+#define OUTPUT_MASK ((1 << LED_BLUE) | (1 << LED_RED) | (1 << LED_GREEN))
+#define INPUT_MASK ((1 << JOYSTICK_BUTTON) | (1 << BUTTON_A))
+
+uint blue_pwm_slice, red_pwm_slice;
 uint wrap = 2047;
+uint32_t last_time = 0;
+bool pwm_state = true;
+
+
+// Função de interrupção da GPIO
+void gpio_irq_handler(uint gpio, uint32_t events){
+    // Obtendo tempo atual (em us)
+    uint32_t current_time = to_us_since_boot(get_absolute_time());
+    if(current_time - last_time > 200000){
+        last_time = current_time; 
+
+        if(gpio == BUTTON_A){
+            pwm_state = !pwm_state; // Alterna o booleano do estado do pwm dos leds
+
+            pwm_set_enabled(blue_pwm_slice, pwm_state);
+            pwm_set_enabled(red_pwm_slice, pwm_state);
+        }
+
+        else if(gpio == JOYSTICK_BUTTON){
+            gpio_put(LED_GREEN, !gpio_get(LED_GREEN));
+        }
+    }
+}
 
 uint led_pwm_init(uint gpio, uint wrap){
     gpio_set_function(gpio, GPIO_FUNC_PWM);
     uint slice_num = pwm_gpio_to_slice_num(gpio);
     pwm_set_clkdiv(slice_num, 125);
     pwm_set_wrap(slice_num, wrap);
-    pwm_set_enabled(slice_num, true);
+    pwm_set_enabled(slice_num, true); 
     return slice_num;
 }
 
@@ -36,19 +66,26 @@ int main(){
     adc_gpio_init(JOYSTICK_Y); // Canal 0
 
     // Habilitando os LEDs
-    gpio_init(LED_BLUE);
-    gpio_set_dir(LED_BLUE, GPIO_OUT);
-    gpio_put(LED_BLUE, 0);
-    gpio_init(LED_RED);
-    gpio_set_dir(LED_RED, GPIO_OUT);
-    gpio_put(LED_RED, 0);
+    gpio_init_mask(OUTPUT_MASK);
+    gpio_set_dir_out_masked(OUTPUT_MASK);
+    gpio_clr_mask(OUTPUT_MASK);
 
     // Iniciando o PWM com 0% de duty
-    uint blue_pwm_slice = led_pwm_init(LED_BLUE, wrap);
-    uint red_pwm_slice = led_pwm_init(LED_RED, wrap);
+    blue_pwm_slice = led_pwm_init(LED_BLUE, wrap);
+    red_pwm_slice = led_pwm_init(LED_RED, wrap);
 
     pwm_set_gpio_level(LED_BLUE, 2047);
     pwm_set_gpio_level(LED_RED, 2047);
+
+    // Iniciando os pinos dos botões
+    gpio_init_mask(INPUT_MASK);
+    // Habilitando os pull ups internos
+    gpio_pull_up(BUTTON_A);
+    gpio_pull_up(JOYSTICK_BUTTON);
+
+    // Configurando a interrupção de GPIO
+    gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    gpio_set_irq_enabled_with_callback(JOYSTICK_BUTTON, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
 
     while (true){
         // Leitura do Eixo X (Canal 1)
